e0cde27e109f6b76316d9c436f032e519898b0cc
Fankai Liu <liufankai137@outlook.com>
2023-11-07 19:35:08 +0800

    fix bug i18n

--- src/i18n.rs
+++ src/i18n.rs
@@ -1,7 +1,9 @@
-use rust_i18n::i18n;
+const SUPPORTED_LANGUAGES: [&str; 17] = [
+    "en", "zh_CN", "zh_TW", "fr", "ja", "es", "de", "ru", 
+    "it", "pt", "ko", "no", "is", "uk", "th", "el", "da"
+];
 
 pub fn set_locale() {
-    i18n!("locales", fallback = "en");
     match get_language() {
         Some(lang) => {
             rust_i18n::set_locale(lang.as_str());
@@ -11,11 +13,16 @@ pub fn set_locale() {
         }
     }
 }
+
 fn get_language() -> Option<String> {
     match std::env::var("LANG") {
         Ok(lang) => {
-            let language = lang.split('.').next();
-            language.map(|s| s.to_string())
+            let language = lang.split('.').next()?.to_string();
+            if SUPPORTED_LANGUAGES.contains(&language.as_str()) {
+                Some(language)
+            } else {
+                None
+            }
         }
         Err(_) => None,
     }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use i18n::set_locale;
 mod i18n;
 rust_i18n::i18n!("locales");
 #[derive(Parser, Debug)]
-#[clap(version = "0.0.1", author = "Fankai liu <liufankai137@outlook.com>")]
+#[clap(version = "0.1.17", author = "Fankai liu <liufankai137@outlook.com>")]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
