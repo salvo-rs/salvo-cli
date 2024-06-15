f6e9b0f32d42e34527f55cd4129c74254f0abeda
Fankai Liu <liufankai137@outlook.com>
2024-06-15 22:12:48 +0800

    fix: fix doc about mysql sql

--- src/template/data/table_mysql.sql
+++ src/template/data/table_mysql.sql
@@ -5,5 +5,5 @@ username VARCHAR(255) NOT NULL UNIQUE,
 password VARCHAR(511) NOT NULL
 );
 BEGIN;
-INSERT IGNORE INTO "users" ("id", "username", "password") VALUES ('cdd0e080-5bb1-4442-b6f7-2ba60dbd0555', 'zhangsan', '$argon2id$v=19$m=19456,t=2,p=1$rcosL5pOPdA2c7i4ZuLA4Q$s0JGh78UzMmu1qZMpVUA3b8kWYLXcZhw7uBfwhYDJ4A');
+INSERT INTO users (id, username, password) VALUES ('cdd0e080-5bb1-4442-b6f7-2ba60dbd0555', 'zhangsan', '$argon2id$v=19$m=19456,t=2,p=1$rcosL5pOPdA2c7i4ZuLA4Q$s0JGh78UzMmu1qZMpVUA3b8kWYLXcZhw7uBfwhYDJ4A');
 COMMIT;
