bd37a63b232439e05a2786e04604ee27bfedc287
mg3994 <126542773+mg3994@users.noreply.github.com>
2026-03-11 03:59:27 +0530

    Hash password before updating user information (#38)
    /api/users/{user_id}
    [PUT]

--- templates/classic/sqlx/src/routers/user.rs
+++ templates/classic/sqlx/src/routers/user.rs
@@ -81,6 +81,7 @@ pub async fn update_user(
 ) -> JsonResult<SafeUser> {
     let user_id = user_id.into_inner();
     let UpdateInData { username, password } = idata.into_inner();
+    let hashed_password = utils::hash_password(&password)?;
     let conn = db::pool();
     let _ = sqlx::query!(
         r#"
@@ -89,7 +90,7 @@ pub async fn update_user(
             WHERE id = $3
             "#,
         username,
-        password,
+        hashed_password,
         user_id,
     )
     .execute(conn)
