91520cbe3bc82faef5f0e5edbc5e9ff28ba7378e
Chrislearn Young <chris@acroidea.com>
2026-07-07 13:38:18 +0800

    Fix rbatis pagination template

--- templates/classic/rbatis/src/routers/user.rs
+++ templates/classic/rbatis/src/routers/user.rs
@@ -1,4 +1,3 @@
-use rbatis::impl_select_page;
 use rbatis::plugin::page::PageRequest;
 use rbs::value;
 use rinja::Template;
@@ -111,13 +110,25 @@ pub async fn delete_user(user_id: PathParam<String>) -> EmptyResult {
     empty_ok()
 }
 
-impl_select_page!(User{select_page() =>"
-     if !sql.contains('count(1)'):
-       `order by id desc`"},"users");
+rbatis::pysql_select_page!(select_page() -> User =>
+r#"`select `
+  if do_count == true:
+    `count(1) as count`
+  if do_count == false:
+    `*`
+  ` from users`
+  if do_count == false:
+    ` order by id desc`"#);
 
-impl_select_page!(User{select_page_by_username(username:&str) =>"
-     if username != null && username != '':
-       `where username like #{username}`"},"users");
+rbatis::pysql_select_page!(select_page_by_username(username: &str) -> User =>
+r#"`select `
+  if do_count == true:
+    `count(1) as count`
+  if do_count == false:
+    `*`
+  ` from users where username like #{username}`
+  if do_count == false:
+    ` order by id desc`"#);
 
 #[derive(Debug, Deserialize, Validate, Extractible, ToSchema)]
 #[salvo(extract(default_source(from = "query")))]
@@ -153,11 +164,11 @@ pub async fn list_users(query: &mut Request) -> JsonResult<UserListResponse> {
 
     let page = if let Some(username) = query.username {
         let pattern = format!("%{}%", username);
-        User::select_page_by_username(rb, &page_req, &pattern)
+        select_page_by_username(rb, &page_req, &pattern)
             .await
             .map_err(anyhow::Error::from)?
     } else {
-        User::select_page(rb, &page_req)
+        select_page(rb, &page_req)
             .await
             .map_err(anyhow::Error::from)?
     };
